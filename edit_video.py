changed calibration calculations to average ver calibrations instead of cases; separated the gaze data form the rest of the data in the dataset folder; fixed the resizing of the video for the paper and its audio synchronization; added standard error calculation for fleiss kappa; fixed the ellipse generation for IoU calculation for the paper results; added script to calculate statistics by sex; added script to calculate how much localization specific for each case was available in the eye trcking data; added script to generateheatmaps from the fixations; added script to build table for "other" labels standardizattion

edit_video.py
@@ -10,17 +10,21 @@ from PIL import Image,ImageDraw
 from pydub import AudioSegment
 import json
 import pyttsx3
+import librosa
+import pyrubberband
+import soundfile as sf
+from skimage.transform import resize
+from skimage.util import img_as_ubyte
 
 fps = 30.
 audio_stephen = True
-scale_video = 0.25
+scale_video = 0.175
 
 def clean(get_frame, t):
     frame_t = t
     while frame_t>=0:
         calculated_frame = get_frame(frame_t)
         frame = calculated_frame['frame']
-        print('oi1')
         all_black = calculated_frame['all_black']
         all_white = calculated_frame['all_white']
         all_green = calculated_frame['all_green']
@@ -42,6 +46,7 @@ def scroll(get_frame, t,table_et, delay):
     fixations = fixations[t>=(fixations['time_start_linux']-delay)]
     if len(fixations)>0:
         frame = draw_circle(frame,scale_video*fixations['position_x'].values[0], scale_video*fixations['position_y'].values[0], scale_video*fixations['angular_resolution_x'].values[0],scale_video*fixations['angular_resolution_y'].values[0], (255,0,0))
+    
     samples = table_et[table_et['type']=='sample']
     # print((abs(samples['time_start_linux']-delay-t)).min())
     samples = samples.iloc[abs(samples['time_start_linux']-delay-t).argsort()[:2]]
@@ -156,10 +161,11 @@ def ASC2CSV(fname):
 
 import copy
 def fl(clip, fun,filter_bad_frames):
+    calculated_frames = []
+    i = 0
     print(len(list(clip.iter_frames())))
-
-    def my_get_frame(t):
-        frame = clip.get_frame(t)
+    for frame in clip.iter_frames():
+        i+=1
         if filter_bad_frames:
             top_row = (frame[0,...,2]>=60).sum()
             bottom_row = (frame[0,...,2]>=60).sum()
@@ -174,11 +180,24 @@ def fl(clip, fun,filter_bad_frames):
             all_black = 0
             all_green = 0
             all_black = 0
-        from skimage.transform import resize
+        print('oi1')
+        print(frame.shape)
+        print(frame.dtype)
+        print(frame.max())
         frame = resize(frame.copy(), (int(frame.shape[0] *scale_video), int(frame.shape[1] *scale_video)),
                            anti_aliasing=True)
-        calculated_frames = {'frame':frame, 'all_black':all_black,'all_green':all_green,'all_white':all_white,'top_row':top_row,'bottom_row':bottom_row}
-        return calculated_frames
+        frame = img_as_ubyte(frame)
+        # frame = frame.astype(np.uint8)
+        print(frame.shape)
+        print(frame.dtype)
+        print(frame.max())
+        calculated_frames.append({'frame':frame, 'all_black':all_black,'all_green':all_green,'all_white':all_white,'top_row':top_row,'bottom_row':bottom_row})
+    
+    def my_get_frame(t):
+        index = round(clip.fps*t)
+        if index>=len(calculated_frames):
+            index -= 1
+        return calculated_frames[index]
     
     clip = clip.set_make_frame(lambda t: fun(my_get_frame, t))
     return clip
@@ -189,28 +208,28 @@ def trim_audio(path_to_wav, start_trim, end_trim):
     trimmed_sound = sound[start_trim*1000:duration-end_trim*1000]
     trimmed_sound.export(path_to_wav, format="wav")
 
-if audio_stephen:
-    class SaveTTSFile:
-        def __init__(self, filename):
-            self.engine = pyttsx3.init()
-            self.filename = filename
 
-        def start(self,text, start,end):
-            self.engine.setProperty('rate', 60/(end-start))
-            self.engine.save_to_file(text , self.filename)
-            self.engine.runAndWait()
+class SaveTTSFile:
+    def __init__(self, filename):
+        self.engine = pyttsx3.init()
+        self.filename = filename
 
-    def stretch_audio(audio, filepath, stretch_constant):
-        audio.export(filepath, format="wav")
-        y, sr = librosa.load(filepath, sr=None)
-        y_stretched = pyrubberband.time_stretch(y, sr, stretch_constant)
-        sf.write(filepath, y_stretched, sr, format='wav')
-        return AudioSegment.from_file(filepath, format="wav")
+    def start(self,text, start,end):
+        self.engine.setProperty('rate', 60/(end-start))
+        self.engine.save_to_file(text , self.filename)
+        self.engine.runAndWait()
+
+def stretch_audio(audio, filepath, stretch_constant):
+    audio.export(filepath, format="wav")
+    y, sr = librosa.load(filepath, sr=None)
+    y_stretched = pyrubberband.time_stretch(y, sr, stretch_constant)
+    sf.write(filepath, y_stretched, sr, format='wav')
+    return AudioSegment.from_file(filepath, format="wav")
 
 def main():
     extensions= ['ogv','mov']
     trial = ['326','160']
-    folders = ['collected_data/video/302_20210514-143755-6691','collected_data/video/305_20210514-141912-2142']
+    folders = ['video_302_20210514-143755-6691','video_305_20210514-141912-2142']
     filter_bad_frames = [True,False]
     for index_trial in [0,1]:
         results_df = pd.read_csv(f'{folders[index_trial]}/structured_output.csv')
@@ -235,11 +254,19 @@ def main():
                             previous_end = 0
                             with open(f'{folders[index_trial]}/{trial[index_trial]}_joined.json','r') as f:
                                 table_text = json.load(f)['timestamps']
+                            with open(f'{folders[index_trial]}/{trial[index_trial]}_trim.json','r') as f:
+                                b = json.load(f)
+                            trim_value = float(b['start_trim'])/1000
                             for row in table_text:
+                                print(row[1])
+                                print(trim_value)
+                                row[1] += trim_value
+                                row[2] += trim_value
+                                print(row[1])
                                 if row[1] == row[2]:
                                     continue
                                 tts = SaveTTSFile('create_video_temp.mp3')
-                                tts.start(row[0].replace('.', 'period').replace(',','comma').replace('/', 'slash') , row['timestamp_start_word'], row['timestamp_end_word'])
+                                tts.start(row[0].replace('.', 'period').replace(',','comma').replace('/', 'slash') , row[1], row[2])
                                 del(tts)
                                 if row[1]>previous_end:
                                     full_audio += AudioSegment.silent(duration=(row[1]-previous_end)*1000)
@@ -247,7 +274,7 @@ def main():
                                 print(row[1])
                                 assert(abs(full_audio.duration_seconds - row[1])<0.002)
                                 word_audio = AudioSegment.from_file('create_video_temp.mp3', format="mp3")
-                                word_audio = stretch_audio(word_audio, 'create_video_temp.mp3', word_audio.duration_seconds/(row['timestamp_end_word'] - row['timestamp_start_word']))
+                                word_audio = stretch_audio(word_audio, 'create_video_temp.mp3', word_audio.duration_seconds/(row[2] - row[1]))
                                 full_audio += word_audio
                                 assert(abs(full_audio.duration_seconds - row[2])<0.002)
                                 previous_end = row[2]
@@ -256,18 +283,18 @@ def main():
                         else:
                             audio_background = mpe.AudioFileClip(f'{folders[index_trial]}/{trial[index_trial]}.wav')
                             # delay_audio = round(delay_audio*my_clip.fps)/my_clip.fps
-                            if delay_audio>0:
-                                null_audio = mpe.AudioClip(lambda t: 0, duration= delay_audio)
-                                audio_background = mpe.concatenate_audioclips([null_audio,audio_background])
-                                delay_audio = 0
-                            delay_end_video = my_clip.duration - audio_background.duration
-                            if delay_end_video>0:
-                                 null_audio = mpe.AudioClip(lambda t: 0, duration= delay_end_video)
-                                 audio_background = mpe.concatenate_audioclips([audio_background, null_audio])
-                                 delay_end_video = 0
-                            audio_background.write_audiofile('temp_crop_audio.wav')
-                            trim_audio('temp_crop_audio.wav', -delay_audio, -delay_end_video)
-                            audio_background = mpe.AudioFileClip('temp_crop_audio.wav')
+                        if delay_audio>0:
+                            null_audio = mpe.AudioClip(lambda t: 0, duration= delay_audio)
+                            audio_background = mpe.concatenate_audioclips([null_audio,audio_background])
+                            delay_audio = 0
+                        delay_end_video = my_clip.duration - audio_background.duration
+                        if delay_end_video>0:
+                             null_audio = mpe.AudioClip(lambda t: 0, duration= delay_end_video)
+                             audio_background = mpe.concatenate_audioclips([audio_background, null_audio])
+                             delay_end_video = 0
+                        audio_background.write_audiofile('temp_crop_audio.wav')
+                        trim_audio('temp_crop_audio.wav', -delay_audio, -delay_end_video)
+                        audio_background = mpe.AudioFileClip('temp_crop_audio.wav')
                         
                     else:
                         if screen==4:

interrater.py
@@ -7,13 +7,20 @@ from nltk import agreement
 import csv
 from sklearn.metrics import roc_auc_score, confusion_matrix
 from statsmodels.stats import inter_rater
+import math
 
-dataset_location = 'built_dataset'
+dataset_location = 'built_dataset/main_data/'
 
 eng = []
 import shapely
 import shapely.geometry as geometry
-def create_ellipse(rect):
+def create_ellipse(coords_box_):
+    coords_box = []
+    coords_box.append((coords_box_[0]+coords_box_[2])/2)
+    coords_box.append((coords_box_[1]+coords_box_[3])/2)
+    coords_box.append(abs(coords_box_[0]-coords_box_[2])/2)
+    coords_box.append(abs(coords_box_[1]-coords_box_[3])/2)
+    rect = coords_box
     center = (rect[0],rect[1])
     axis = (rect[2],rect[3])
     point = geometry.point.Point(center).buffer(1)
@@ -45,6 +52,16 @@ def convert_to_stats_table(array, total_raters):
     array_to_return[:,1] = total_raters - array.sum(axis = 1)
     return array_to_return
 
+def fleiss_kappa_standard_error(table):
+    n_raters = np.sum(table[0,:])
+    n_categories = table.shape[1]
+    n_cases = table.shape[0]
+    p = np.sum(table, axis = 0)/n_raters/n_cases
+    sum_p2 = np.sum(p**2)
+    sum_p3 = np.sum(p**3)
+    var_k = 2/n_cases/n_raters/(n_raters-1)*(sum_p2-(2*n_raters-3)*sum_p2**2+2*(n_raters-2)*sum_p3)/(1-sum_p2)**2
+    return math.sqrt(var_k)
+
 def analyze_interrater_reliability(phase, labels):
     results_csv = pd.DataFrame()
     labels_list = labels
@@ -71,7 +88,11 @@ def analyze_interrater_reliability(phase, labels):
         
         # numpy.savetxt(f"rating_{labels[k].replace('/','_').replace(' ','_').lower()}_phase_{phase}.csv", convert_to_stats_table(array_to_use[:,:,k], 5), delimiter=",")
         # numpy.savetxt(f"rating_2_{labels[k].replace('/','_').replace(' ','_').lower()}_phase_{phase}.csv", array_to_use[:,:,k], delimiter=",")
-        value = inter_rater.fleiss_kappa(convert_to_stats_table(array_to_use[:,:,k], 5), method='fleiss')
+        table_answers = convert_to_stats_table(array_to_use[:,:,k], 5)
+        value = inter_rater.fleiss_kappa(table_answers, method='fleiss')
+        
+        se = fleiss_kappa_standard_error(table_answers)
+        
         
         # formatted_codes = [[j,i,array_to_use[j,i,k]] for i in range(array_to_use.shape[1]) for j in range(array_to_use.shape[0])]
         # 
@@ -86,6 +107,8 @@ def analyze_interrater_reliability(phase, labels):
         # print(value)
         # 1/0
         new_row = {'label':labels_list[k], 'title':'Fleiss Kappa', 'value':value}
+        results_csv = results_csv.append(new_row, ignore_index=True)
+        new_row = {'label':labels_list[k], 'title':'Fleiss Kappa standard error', 'value':se}
         results_csv = results_csv.append(new_row, ignore_index=True)
     
         for trial_index,_ in enumerate(all_images):
@@ -226,7 +249,7 @@ def calculate_per_user(name, results_csv, formatted_codes, formatted_codes_other
     
     return results_csv
 
-# labels = sorted(['Atelectasis','Consolidation','Pulmonary edema','Airway wall thickening','Groundglass opacity','Mass','Nodule','Pneumothorax','Pleural effusion','Pleural thickening','Emphysema','Fibrosis','Wide mediastinum','Enlarged cardiac silhouette','Support devices','Fracture','Quality issue'])
-# analyze_interrater_reliability(1, labels)
+labels = sorted(['Atelectasis','Consolidation','Pulmonary edema','Airway wall thickening','Groundglass opacity','Mass','Nodule','Pneumothorax','Pleural effusion','Pleural thickening','Emphysema','Fibrosis','Wide mediastinum','Enlarged cardiac silhouette','Support devices','Fracture','Quality issue'])
+analyze_interrater_reliability(1, labels)
 labels = sorted(['Support devices', 'Abnormal mediastinal contour', 'Enlarged cardiac silhouette', 'Enlarged hilum', 'Hiatal hernia', 'Pneumothorax', 'Pleural abnormality', 'Consolidation','Groundglass opacity','Atelectasis', 'Lung nodule or mass', 'Pulmonary edema', 'High lung volume / emphysema','Interstitial lung disease', 'Acute fracture'])
 analyze_interrater_reliability(2, labels)